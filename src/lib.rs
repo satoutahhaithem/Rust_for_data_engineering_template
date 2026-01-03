111429991430d9fb276f756b3fceae7b4249373b
Haithem Satoutah <82703504+satoutahhaithem@users.noreply.github.com>
2026-01-03 14:20:59 +0000

    add calc op

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+pub fn mul(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+pub fn div(a: i32, b: i32) -> i32 {
+    a / b
+}
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use trsut::{add, mul, div};
+
 fn main() {
     println!("Hello, world!");
+    
+    let sum = add(10, 5);
+    println!("10 + 5 = {}", sum);
+    
+    let product = mul(10, 5);
+    println!("10 * 5 = {}", product);
+    
+    let quotient = div(10, 5);
+    println!("10 / 5 = {}", quotient);
 }
